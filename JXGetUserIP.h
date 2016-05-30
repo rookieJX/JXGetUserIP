d03d120fc34b3aaebd77236a0f024f30799844e3
Throne <wangjiaxiangyx@163.com>
2016-05-30 13:33:46 +0800

    获取设备当前IP地址
    获取设备当前IP地址

---
+++ JXGetUserIP.h
@@ -0,0 +1,13 @@
+//
+//  JXGetUserIP.h
+//  JXGetIP
+//
+//  Created by yuezuo on 16/5/30.
+//  Copyright © 2016年 yuezuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JXGetUserIP : NSObject
++ (NSString *)getIpAddresses;
+@end
